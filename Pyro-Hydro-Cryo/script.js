e8e8c3d726eedaca6473da19a88ccc24091822fb
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-06 20:36:18 +0800

    Create enemyAttack() function

--- Pyro-Hydro-Cryo/script.js
+++ Pyro-Hydro-Cryo/script.js
@@ -6,3 +6,14 @@ const enemyPyro = document.getElementById('enemy-pyro');
 const enemyHydro = document.getElementById('enemy-hydro');
 const enemyCryo = document.getElementById('enemy-cryo');
 
+function enemyAttack() {
+  enemyElement = Math.floor(Math.random()*3) + 1;
+
+  switch (enemyElement) {
+    case 1: return 'Pyro';
+    case 2: return 'Hydro';
+    case 3: return 'Cryo';
+  }
+}
+
+console.log(enemyAttack());
