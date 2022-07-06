0a633a722724cd09c54cf7633830177ba8c433d6
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-06 20:26:24 +0800

    Instantiate attack element in .js file

---
+++ Pyro-Hydro-Cryo/script.js
@@ -0,0 +1,8 @@
+const playerPyro = document.getElementById('player-pyro');
+const playerHydro = document.getElementById('player-hydro');
+const playerCryo = document.getElementById('player-cryo');
+
+const enemyPyro = document.getElementById('enemy-pyro');
+const enemyHydro = document.getElementById('enemy-hydro');
+const enemyCryo = document.getElementById('enemy-cryo');
+
